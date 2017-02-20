069a4ff5771d8adc9e6490042a6a89bf7b09e104
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-02-19 22:19:47 -0500

    Extract functions for encrypting and decrypting a buffer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -7,27 +7,44 @@ export default class App extends React.Component {
     this.refs.input.focus()
   }
 
-  onInputChange = async ({target: {value}}) => {
-    await this.setState({data: parseInt(value)});
-
-    let buffer = new ArrayBuffer(4);
-    let dataView = new DataView(buffer);
-
-    dataView.setUint8(0, value);
-
+  async encrypt(buffer) {
     let iv = window.crypto.getRandomValues(new Uint8Array(12));
     let key = await window.crypto.subtle.generateKey({name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"]);
-    let encrypted = await window.crypto.subtle.encrypt({name: "AES-GCM", iv: iv}, key, dataView);
-
-    this.setState({encrypted: new DataView(encrypted).getUint8()});
-
+    let encrypted = await window.crypto.subtle.encrypt({name: "AES-GCM", iv: iv}, key, buffer);
     let jwk = await window.crypto.subtle.exportKey("jwk", key);
 
-    let importedKey = await window.crypto.subtle.importKey("jwk", {kty: "oct", k: jwk.k, alg: "A256GCM", ext: true}, {name: "AES-GCM"}, false, ["encrypt", "decrypt"]);
+    return {iv, jwk: jwk.k, encrypted}
+  }
 
-    let decrypted = await window.crypto.subtle.decrypt({name: "AES-GCM", iv: iv}, importedKey, encrypted);
+  async decrypt(buffer, jwk, iv) {
+    let importedKey = await window.crypto.subtle.importKey("jwk", {kty: "oct", k: jwk, alg: "A256GCM", ext: true}, {name: "AES-GCM"}, false, ["encrypt", "decrypt"]);
+    let decrypted = await window.crypto.subtle.decrypt({name: "AES-GCM", iv: iv}, importedKey, buffer);
 
-    this.setState({decrypted: new DataView(decrypted).getUint8()});
+    return decrypted
+  }
+
+  onInputChange = async ({target: {value}}) => {
+    let dataView = new DataView(new ArrayBuffer(4));
+    dataView.setUint8(0, value);
+
+    let {iv, jwk, encrypted} = await this.encrypt(dataView);
+    let decrypted = await this.decrypt(encrypted, jwk, iv);
+
+    this.setState({
+      data: parseInt(value),
+      encrypted: new DataView(encrypted).getUint8(),
+      decrypted: new DataView(decrypted).getUint8()
+    });
+  };
+
+  onFileInputChange = async ({target: {files}}) => {
+    let f = files[0];
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(f);
+    await new Promise((resolve) => setTimeout(() => {if(reader.readyState == 2) resolve()}, 100));
+    let {result} = reader;
+    let encrypted = await this.encrypt(result);
+    console.log(encrypted)
   };
 
   render() {
@@ -36,6 +53,9 @@ export default class App extends React.Component {
         <input ref='input' value={this.state.data} onChange={this.onInputChange} />
         <span>{this.state.encrypted}</span>
         <span style={{marginLeft: '10px'}}>{this.state.decrypted}</span>
+
+        <div />
+        <input type='file' onChange={this.onFileInputChange}/>
       </div>
     );
   }
