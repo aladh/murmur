fead55c2ceed0f796892df3e3ebb429a7b4edd9e
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-02-19 21:32:52 -0500

    Export key to json web key, import it back, and then decrypt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -16,16 +16,18 @@ export default class App extends React.Component {
     dataView.setUint8(0, value);
 
     let iv = window.crypto.getRandomValues(new Uint8Array(12));
-    console.log(iv)
-    let key = await window.crypto.subtle.generateKey({name: "AES-GCM", length: 256}, false, ["encrypt", "decrypt"]);
-    console.log(key)
+    let key = await window.crypto.subtle.generateKey({name: "AES-GCM", length: 256}, true, ["encrypt", "decrypt"]);
     let encrypted = await window.crypto.subtle.encrypt({name: "AES-GCM", iv: iv}, key, dataView);
 
-    this.setState({encrypted: new DataView(encrypted).getUint8()})
+    this.setState({encrypted: new DataView(encrypted).getUint8()});
 
-    let decrypted = await window.crypto.subtle.decrypt({name: "AES-GCM", iv: iv}, key, encrypted)
+    let jwk = await window.crypto.subtle.exportKey("jwk", key);
 
-    this.setState({decrypted: new DataView(decrypted).getUint8()})
+    let importedKey = await window.crypto.subtle.importKey("jwk", {kty: "oct", k: jwk.k, alg: "A256GCM", ext: true}, {name: "AES-GCM"}, false, ["encrypt", "decrypt"]);
+
+    let decrypted = await window.crypto.subtle.decrypt({name: "AES-GCM", iv: iv}, importedKey, encrypted);
+
+    this.setState({decrypted: new DataView(decrypted).getUint8()});
   };
 
   render() {
