d6cdf8078f39f90865fdd948e2550a75b5b724c6
Alishan Ladhani <alishan@retailcommon.com>
2017-02-24 15:00:45 -0500

    Store access token in secret file

--- app/components/UploadPage.jsx
+++ app/components/UploadPage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Dropbox from 'dropbox';
 import utils from './utils';
 import dropbox from './dropbox';
+import secrets from '../secrets';
 
 export default class UploadPage extends React.Component {
-  dbx = new Dropbox({accessToken: ''});
+  dbx = new Dropbox({accessToken: secrets.dropboxAccessToken});
   state = {linkId: '', key: ''};
 
   uploadFile = async ({target: {files}}) => {
