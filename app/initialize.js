68aae6e7d7946b62dfd1c62c95261e4e4a042380
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-02-26 03:31:45 -0500

    Add google analytics tracking id

--- app/initialize.js
+++ app/initialize.js
@@ -2,6 +2,8 @@ import "babel-polyfill";
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from 'components/App';
+import secrets from './secrets';
+window.gaTrackingId = secrets.gaTrackingId;
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.querySelector('#app'));
