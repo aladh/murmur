599aa498c4e52199214906534c40a24635ba9e4a
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-02-26 19:55:10 -0500

    Add Bugsnag error tracking

--- app/initialize.js
+++ app/initialize.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from 'components/App';
 import secrets from './secrets';
+
 window.gaTrackingId = secrets.gaTrackingId;
+
+Bugsnag.apiKey = secrets.bugsnagApiKey;
+Bugsnag.notifyReleaseStages = ["production"];
+if (location.hostname == 'localhost') Bugsnag.releaseStage = "development";
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.querySelector('#app'));
