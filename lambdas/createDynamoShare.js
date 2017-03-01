f7cd60c974827803f7eab40bb33414e10eac2ca5
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-02-28 23:44:44 -0500

    Lambda function to create share

---
+++ lambdas/createDynamoShare.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const sharesTable = require('resources/SharesTable');
+
+exports.handler = (event, context, callback) => {
+	let body = JSON.parse(event.body);
+	let item = {
+		id: body.id,
+		iv: new Uint8Array(body.iv),
+		fileName: body.fileName,
+		accessToken: body.accessToken
+	};
+
+	sharesTable.putItem(item.id, item.iv, item.fileName, item.accessToken)
+		.then(() => {
+	    context.succeed({
+	      "statusCode": 200,
+		    "headers": {},
+	      "body": "" 
+	    })
+		})
+		.catch(err => {
+	    context.succeed({
+	      "statusCode": 500,
+	      "headers": {},
+	      "body": ""
+	    })
+		})
+};
