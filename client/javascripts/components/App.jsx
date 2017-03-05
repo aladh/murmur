f5a4906f8c85234ea7ee506159f62de9c10bf417
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-03-05 18:59:42 -0500

    React to changes in URL

--- client/javascripts/components/App.jsx
+++ client/javascripts/components/App.jsx
@@ -4,19 +4,29 @@ import DownloadPage from './DownloadPage';
 import AuthPage from './AuthPage';
 
 export default class App extends React.Component {
-  params = new URLSearchParams(location.hash.slice(1));
+  state = this.stateFromParams();
+
+  stateFromParams() {
+    return {
+      params: new URLSearchParams(location.hash.slice(1))
+    }
+  }
 
   routeToPage() {
-    let dropboxAccessToken = this.params.get('access_token');
-    let shareId = this.params.get('share');
+    let dropboxAccessToken = this.state.params.get('access_token');
+    let shareId = this.state.params.get('share');
 
     if(shareId) {
-      return <DownloadPage shareId={shareId} jwk={this.params.get('key')} />
+      return <DownloadPage shareId={shareId} jwk={this.state.params.get('key')} />
     } else if(!dropboxAccessToken) {
       return <AuthPage />
     } else {
       return <UploadPage dropboxAccessToken={dropboxAccessToken} />
     }
+  }
+
+  componentDidMount() {
+    window.onpopstate = () => this.setState(this.stateFromParams())
   }
 
   render() {
