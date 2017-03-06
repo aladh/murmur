997a80ee05fe6e0d684e577e2f18cc755dc95272
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-03-05 19:26:54 -0500

    Inline styles for headers outside of react

--- client/javascripts/components/App.jsx
+++ client/javascripts/components/App.jsx
@@ -31,7 +31,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div id="content">
+      <div className="app">
         {this.routeToPage()}
       </div>
     );
