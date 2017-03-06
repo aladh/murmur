878cc8a6bde420bb5386f5be3f36fb9a9c80c31d
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-03-05 22:18:50 -0500

    Fix naming issue

--- server/deleteDropboxFile.js
+++ server/deleteDropboxFile.js
@@ -1,8 +1,8 @@
-import deleteDropboxFile from './resources/dropbox';
+import dropbox from './resources/dropbox';
 
 const handler = (event, context, callback) => {
   event.Records.forEach(record => {
-    if (record.eventName == 'REMOVE') deleteDropboxFile(record.dynamodb.OldImage.accessToken.S, record.dynamodb.OldImage.fileName.S)
+    if (record.eventName == 'REMOVE') dropbox.deleteFile(record.dynamodb.OldImage.accessToken.S, record.dynamodb.OldImage.fileName.S)
   });
 
   callback(null, `Successfully processed ${event.Records.length} records.`);
