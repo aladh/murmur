1865901c050ecfcc726da3ba7a4f491032197e3a
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-03-07 22:11:43 -0500

    Refactor and add error logging

--- server/lambdas/deleteDropboxFile.js
+++ server/lambdas/deleteDropboxFile.js
@@ -1,10 +1,19 @@
 import 'babel-polyfill';
 import dropbox from '../../shared/dropbox';
+import bugsnag from 'bugsnag';
+import secrets from '../secrets';
 
-export default (event, context, callback) => {
-  event.Records.forEach(record => {
-    if (record.eventName == 'REMOVE') dropbox.deleteFile(record.dynamodb.OldImage.accessToken.S, record.dynamodb.OldImage.fileName.S)
-  });
+export default ({Records}, context, callback) => {
+  bugsnag.register(secrets.bugsnagApiKey);
 
-  callback(null, `Successfully processed ${event.Records.length} records.`);
+  try {
+    Records.forEach(record => {
+      if (record.eventName == 'REMOVE') dropbox.deleteFile(record.dynamodb.OldImage.accessToken.S, record.dynamodb.OldImage.fileName.S)
+    });
+
+    callback(null, `Successfully processed ${Records.length} records.`);
+  } catch(e) {
+    bugsnag.notify(e);
+    console.error(`Failed to process records: ${JSON.stringify(Records)}`)
+  }
 }
