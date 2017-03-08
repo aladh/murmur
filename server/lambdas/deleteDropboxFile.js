717a0814b85f581e564fe6ca684516a6903f7b26
Alishan Ladhani <alishan.ladhani@gmail.com>
2017-03-07 21:58:19 -0500

    Add babel polyfill to fix regenerator runtime not defined. Use default export

--- server/lambdas/deleteDropboxFile.js
+++ server/lambdas/deleteDropboxFile.js
@@ -1,11 +1,10 @@
+import 'babel-polyfill';
 import dropbox from '../../shared/dropbox';
 
-const handler = (event, context, callback) => {
+export default (event, context, callback) => {
   event.Records.forEach(record => {
     if (record.eventName == 'REMOVE') dropbox.deleteFile(record.dynamodb.OldImage.accessToken.S, record.dynamodb.OldImage.fileName.S)
   });
 
   callback(null, `Successfully processed ${event.Records.length} records.`);
-};
-
-export {handler as handler}
+}
